00fb17c0cac4f646a3a88534576a84b5f1b35271
Aleksander Klak <258957@student.pwr.edu.pl>
2023-05-20 20:58:19 +0200

    dodanie wektora

--- operatory/wektor.h
+++ operatory/wektor.h
@@ -1,5 +1,6 @@
 #ifndef WEKTOR_H
 #define WEKTOR_H
+#include <iostream>
 
 class Vector3D
 {
@@ -20,8 +21,13 @@ public:
     // Metoda do wyświetlania wektora
     void print() const;
 
-    // Metoda do dodawania dwóch wektorów
-    Vector3D add(const Vector3D &other) const;
+    // Przeciążone operatory
+    Vector3D operator+(const Vector3D &other) const;
+    Vector3D operator-(const Vector3D &other) const;
+    Vector3D operator*(const Vector3D &other) const;
+    Vector3D operator*(double scalar) const;
+    double operator[](int index) const;
+    friend std::ostream &operator<<(std::ostream &os, const Vector3D &vector);
 
     // Metoda do obliczania długości wektora
     double length() const;
